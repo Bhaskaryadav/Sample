36fa30d8b925db848fbce8bf730e3e6b5be23f0d
admin <admin@DESKTOP-GAFCIHF>
2019-09-16 00:15:14 +0530

    commiting sample data

---
+++ RetriveDataFromDatabase/src/SampleData.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class SampleData {
+
+	public static void main(String[] args) {
+		System.out.println("Inside Method");
+		
+		Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+		
+		try{  
+			//step1 load the driver class  
+			Class.forName("oracle.jdbc.driver.OracleDriver");  
+			  
+			//step2 create  the connection object  
+			 connection=DriverManager.getConnection(  
+			"jdbc:oracle:thin:@localhost:1521:xe","system","bhaskar");  
+			  
+			String sqlSelectQuery = "select * from customer";
+			//step3 create the statement object  
+			preparedStatement = connection.prepareStatement(sqlSelectQuery );  
+			  
+			//step4 execute query  
+			rs = preparedStatement.executeQuery();
+			int count=0;
+			while(rs.next())  
+				System.out.println(rs.getInt(1)+"  "+rs.getInt(2)+"  "+rs.getInt(3)+"  "+rs.getString(4));
+				count=rs.getRow();
+			System.out.println("Number of rows retrieved from Table: "+count);			  
+			  
+			//step5 close the connection object  
+			connection.close();  
+			  
+			}catch(Exception e){ System.out.println(e);}  
+			  
+	}
+
+}
