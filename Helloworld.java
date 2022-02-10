54a7104d6f5334366dfeda343c7c8b3a47307fcc
Bhaskaryadav <44144232+Bhaskaryadav@users.noreply.github.com>
2022-02-10 18:30:11 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
 public class Helloworld {
    public static void main(String[] args) {
       System.out.println("This is just Hello world! Program"); 
+      System.out.println("Welcome to Java!!"); 
    }
 }
